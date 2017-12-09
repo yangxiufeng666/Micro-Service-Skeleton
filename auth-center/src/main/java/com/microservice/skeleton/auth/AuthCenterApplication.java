3feeb8bf951a97bd2d93b642627410b521dcb673
ManongJu <469863752@qq.com>
2017-12-09 15:22:06 +0800

    initial auth-center

---
+++ auth-center/src/main/java/com/microservice/skeleton/auth/AuthCenterApplication.java
@@ -0,0 +1,12 @@
+package com.microservice.skeleton.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthCenterApplication.class, args);
+	}
+}
--- gateway/src/main/java/com/microservice/skeleton/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/microservice/skeleton/gateway/GatewayApplication.java
@@ -2,8 +2,12 @@ package com.microservice.skeleton.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulServer
 public class GatewayApplication {
 
 	public static void main(String[] args) {
