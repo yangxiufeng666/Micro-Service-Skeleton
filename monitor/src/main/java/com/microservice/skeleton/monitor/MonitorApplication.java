89a822c8af9148ab71ba2a9d49aa0ae4f6d50210
ManongJu <469863752@qq.com>
2017-12-26 17:40:20 +0800

    Zipkin服务追踪

--- monitor/src/main/java/com/microservice/skeleton/monitor/MonitorApplication.java
+++ monitor/src/main/java/com/microservice/skeleton/monitor/MonitorApplication.java
@@ -5,13 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableFeignClients
 @EnableHystrixDashboard
-//@EnableCircuitBreaker
 @EnableTurbine
+@EnableZipkinServer
 public class MonitorApplication {
 
 	public static void main(String[] args) {
