1495e170723210c85cf08b49816d8eac508315a0
ManongJu <469863752@qq.com>
2017-12-26 15:56:20 +0800

    微服务监控

--- monitor/src/main/java/com/microservice/skeleton/monitor/MonitorApplication.java
+++ monitor/src/main/java/com/microservice/skeleton/monitor/MonitorApplication.java
@@ -2,8 +2,16 @@ package com.microservice.skeleton.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+//@EnableFeignClients
+@EnableHystrixDashboard
+//@EnableCircuitBreaker
+@EnableTurbine
 public class MonitorApplication {
 
 	public static void main(String[] args) {
